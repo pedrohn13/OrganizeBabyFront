290a95227badb27ae3a56630f7b201ad4c09976d
Pedro <pedrohn13@gmail.com>
2016-02-18 14:48:28 -0300

    Criando ComboBox para responsavel

--- app/src/bebe/script/bebe.create.controller.js
+++ app/src/bebe/script/bebe.create.controller.js
@@ -1,52 +1,53 @@
 (function () {
 
-    angular
-        .module('users')
-        .controller('BebeCreateController',
-            ['$http', '$location', BebeCreateController]);
+  angular
+    .module('users')
+    .controller('BebeCreateController',
+      ['$http', '$location', BebeCreateController]);
 
-    function BebeCreateController($http, $location) {
-        var root = 'https://leonardoads.pythonanywhere.com/OrganizeBaby/default/api';
-        var self = this;
-        self.create = create;
-        self.novoBB = {idbercario: 1,
-                      idresponsavel2: 2,
-                      idresponsavel1: 1};
-        $http({
-            method: "GET",
-            url: root + '/pais.json'
-        }).then(function mySucces(response) {
-            self.responsaveis = response.data.content;
-            console.log(self.responsaveis)
-        }, function myError(response) {
-            console.log(response)
-        });
+  function BebeCreateController($http, $location) {
+    var root = 'https://leonardoads.pythonanywhere.com/OrganizeBaby/default/api';
+    var self = this;
+    self.create = create;
+    self.novoBB = {
+      idbercario: 1,
+      idresponsavel2: 2,
+    };
 
-        function create() {
-            $http({
-                method: "POST",
-                url: root + '/bebe.join',
-                data: self.novoBB
-            }).then(function mySucces(response) {
-                $location.path('bebe.list');
-            }, function myError(response) {
-                console.log('fail')
-            });
-        }
+    $http({
+      method: "GET",
+      url: root + '/pais.json'
+    }).then(function mySucces(response) {
+      self.responsaveis = response.data.content;
+    }, function myError(response) {
+    });
 
-        self.querySearch = function(){
-          return $http({
-              method: "GET",
-              url: root + '/pais.json'
-              }).then(function mySucces(response) {
-                  console.log(self.responsaveis)
-                  return response.data.content;
-              }, function myError(response) {
-                  console.log(response)
-                  return {};
-              });
-          }
-
+    function create() {
+      self.novoBB.idresponsavel1 = self.selectedResp.id;
+      $http({
+        method: "POST",
+        url: root + '/bebe.join',
+        data: self.novoBB
+      }).then(function mySucces(response) {
+        $location.path('bebe.list');
+      }, function myError(response) {
+        console.log('fail')
+      });
     }
+
+    self.querySearch = function (searchText) {
+      var results = searchText ? self.responsaveis.filter(createFilterFor(searchText)) : [];
+      return results;
+    }
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(text) {
+        var lowercaseText = angular.lowercase(text.p_nome);
+        return (lowercaseText.indexOf(lowercaseQuery) === 0);
+      };
+    }
+
+  }
 
 })();
